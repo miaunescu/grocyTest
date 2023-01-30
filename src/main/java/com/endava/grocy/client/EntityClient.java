c5872bbd2cb070e34d75b91c0b132bddb9fd02e2
andiordache <andrei-dario.iordache@endava.com>
2023-01-30 10:47:24 +0200

    Added implementation for Stock Overview buttons (Stock journal, Stock Entries, Location Content Sheet)

--- src/main/java/com/endava/grocy/client/EntityClient.java
+++ src/main/java/com/endava/grocy/client/EntityClient.java
@@ -2,6 +2,7 @@ package com.endava.grocy.client;
 
 import com.endava.grocy.model.Entity;
 import com.endava.grocy.model.EntityType;
+import com.endava.grocy.util.EnvReader;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -10,7 +11,7 @@ public class EntityClient extends BaseClient {
     public Response createEntity(EntityType entityType, Entity entity) {
         return
                 //given().filters(new RequestLoggingFilter(), new ResponseLoggingFilter())
-                getBasicRestConfig()
+                getBasicRestConfig(EnvReader.getBasePath())
                         .contentType(ContentType.JSON)
                         .body(entity)
                         .pathParam("entity", entityType)
@@ -18,7 +19,7 @@ public class EntityClient extends BaseClient {
     }
 
     public Response deleteEntityById(EntityType entityType, Integer productId) {
-        return getBasicRestConfig()
+        return getBasicRestConfig(EnvReader.getBasePath())
                 .pathParam("entity", entityType)
                 .pathParam("objectId", productId)
                 .delete("/objects/{entity}/{objectId}");
