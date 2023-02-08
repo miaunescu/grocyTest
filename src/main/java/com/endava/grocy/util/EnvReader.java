0a2949148dadf0b265cdf229c1081bd80c1b39cc
fbeldica <alexandru.beldica@gmail.com>
2023-02-08 10:16:42 +0200

    Add Selenium dependency and make 4 tests using Selenium

--- src/main/java/com/endava/grocy/util/EnvReader.java
+++ src/main/java/com/endava/grocy/util/EnvReader.java
@@ -45,4 +45,34 @@ public class EnvReader {
     public static String getDBPassword() {
         return properties.getProperty("db.password ");
     }
+
+    public static String geBaseUrl() { return properties.getProperty("baseUrl"); }
+
+    public static String getBatteriesOverview() { return properties.getProperty("batteriesOverview"); }
+
+    public static String getJournalButton() { return properties.getProperty("journalButton"); }
+
+    public static String getSaveButton() { return properties.getProperty("saveButton"); }
+
+    public static String getEditNameBattery() { return properties.getProperty("editNameBattery"); }
+
+    public static String getDescriptionBattery() { return properties.getProperty("descriptionBattery"); }
+
+    public static String getUsedInBattery() { return properties.getProperty("usedInBattery"); }
+
+    public static String getChargeInterval() { return properties.getProperty("chargeInterval"); }
+
+    public static String getTile() { return properties.getProperty("title"); }
+
+    public static String getAddButton() { return properties.getProperty("addButton"); }
+
+    public static String getActiveCheck() { return properties.getProperty("activeCheck"); }
+
+    public static String getFrameName() { return properties.getProperty("frameName"); }
+
+    public static String getEditBattery() { return properties.getProperty("editBattery"); }
+
+    public static String getThreeDots() { return properties.getProperty("threeDots"); }
+
+    public static String getNameBattery() { return properties.getProperty("nameBattery"); }
 }
