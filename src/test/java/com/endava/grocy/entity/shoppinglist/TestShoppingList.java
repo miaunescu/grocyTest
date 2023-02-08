9851fe6e94a9f870d5c98932e61a14220afb5a74
Brainworker000 <vasile.mihai00@gmail.com>
2023-02-08 10:32:44 +0200

    Implemented actual work of the Selenium Testing on StockOverview test cases

--- src/test/java/com/endava/grocy/entity/shoppinglist/TestShoppingList.java
+++ src/test/java/com/endava/grocy/entity/shoppinglist/TestShoppingList.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,8 +19,8 @@ public class TestShoppingList extends TestBaseShoppingList {
         Assert.assertEquals(urlStockOverview, "http://3.65.154.68:8089/stockoverview");
 
         driver.findElement(By.xpath("(//a[@class='nav-link discrete-link'])[2]")).click();
-        String urlBatteriesOverview = driver.getCurrentUrl();
-        Assert.assertEquals(urlBatteriesOverview, "http://3.65.154.68:8089/shoppinglist");
+        String urlShoppingListOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlShoppingListOverview, "http://3.65.154.68:8089/shoppinglist");
 
     }
 
@@ -27,10 +30,10 @@ public class TestShoppingList extends TestBaseShoppingList {
         Assert.assertEquals(urlStockOverview, "http://3.65.154.68:8089/stockoverview");
 
         driver.findElement(By.xpath("(//a[@class='nav-link discrete-link'])[2]")).click();
-        String urlBatteriesOverview = driver.getCurrentUrl();
-        Assert.assertEquals(urlBatteriesOverview, "http://3.65.154.68:8089/shoppinglist");
+        String urlShoppingListOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlShoppingListOverview, "http://3.65.154.68:8089/shoppinglist");
 
-        driver.findElement(By.cssSelector("(//*[@id=\"related-links\"])/a[1]([text()='Add item'])")).click();
+        driver.findElement(By.xpath("//*[contains(text(), 'Add item')]")).click();
 
         driver.findElement(By.xpath("//*[@id=\"product_id_text_input\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"product_id_text_input\"]")).getText();
@@ -48,8 +51,8 @@ public class TestShoppingList extends TestBaseShoppingList {
         Assert.assertEquals(urlStockOverview, "http://3.65.154.68:8089/stockoverview");
 
         driver.findElement(By.xpath("(//a[@class='nav-link discrete-link'])[2]")).click();
-        String urlBatteriesOverview = driver.getCurrentUrl();
-        Assert.assertEquals(urlBatteriesOverview, "http://3.65.154.68:8089/shoppinglist");
+        String urlShoppingListOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlShoppingListOverview, "http://3.65.154.68:8089/shoppinglist");
 
         driver.findElement(By.xpath("//a[@id='clear-shopping-list']")).click();
         driver.switchTo().defaultContent();
@@ -61,8 +64,8 @@ public class TestShoppingList extends TestBaseShoppingList {
         Assert.assertEquals(urlStockOverview, "http://3.65.154.68:8089/stockoverview");
 
         driver.findElement(By.xpath("(//a[@class='nav-link discrete-link'])[2]")).click();
-        String urlBatteriesOverview = driver.getCurrentUrl();
-        Assert.assertEquals(urlBatteriesOverview, "http://3.65.154.68:8089/shoppinglist");
+        String urlShoppingListOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlShoppingListOverview, "http://3.65.154.68:8089/shoppinglist");
         //driver.findElement(By.xpath("//*[@id=\"related-links\"]/a[1]")).click();
         //driver.findElement(By.xpath("//*[@id=\"name\"]")).click();
 
@@ -79,4 +82,35 @@ public class TestShoppingList extends TestBaseShoppingList {
         driver.findElement(By.id("name")).sendKeys("My New Shopping List");
         driver.findElement(By.xpath("//button[text()='Save']")).click();
     }
+    @Test
+    public void testShoppingListChange() {
+//        // Wait for the page to load
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shopping-list-table")));
+        String urlStockOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlStockOverview, "http://3.65.154.68:8089/stockoverview");
+
+        driver.findElement(By.xpath("(//a[@class='nav-link discrete-link'])[2]")).click();
+        String urlShoppingListOverview = driver.getCurrentUrl();
+        Assert.assertEquals(urlShoppingListOverview, "http://3.65.154.68:8089/shoppinglist");
+
+        // Find the input field to add a new item to the shopping list
+        WebElement inputField = driver.findElement(By.id("add-to-shopping-list-input"));
+
+        // Enter the item name
+        String itemName = "Test item";
+        inputField.sendKeys(itemName);
+
+        // Click the Add button
+        WebElement addButton = driver.findElement(By.id("add-to-shopping-list-button"));
+        addButton.click();
+
+//        // Wait for the item to appear in the shopping list
+//        wait.until(ExpectedConditions.textToBe(By.xpath("//table[@id='shopping-list-table']/tbody/tr[last()]/td[2]"), itemName));
+
+        // Verify that the item was added to the shopping list
+        WebElement addedItem = driver.findElement(By.xpath("//table[@id='shopping-list-table']/tbody/tr[last()]/td[2]"));
+        Assert.assertEquals(addedItem.getText(), itemName);
+    }
+
 }
